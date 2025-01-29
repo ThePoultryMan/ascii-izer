e13f0b64c9e62a74b332f59d55e327b428a5c6f7
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-01-28 23:20:56 -0500

    feat: basic output

--- src/lib.rs
+++ src/lib.rs
@@ -17,9 +17,45 @@ fn to_bits<P: AsRef<Path>>(path: P) -> Result<Vec<u8>, AsciiError> {
     Ok(buffer)
 }
 
+fn to_chars(bits: Vec<u8>) -> Vec<char> {
+    let mut chars = Vec::with_capacity(bits.len());
+
+    for level in bits {
+        if level > 230 {
+            chars.push(' ');
+        }
+        else if level >= 200 {
+            chars.push('.');
+        }
+        else if level >= 180 {
+            chars.push('*');
+        }
+        else if level >= 160 {
+            chars.push(':');
+        }
+        else if level >= 130 {
+            chars.push('o');
+        }
+        else if level >= 100 {
+            chars.push('&');
+        }
+        else if level >= 70 {
+            chars.push('8');
+        }
+        else if level >= 50 {
+            chars.push('#');
+        }
+        else {
+            chars.push('@');
+        }
+    }
+
+    chars
+}
+
 #[cfg(test)]
 mod test {
-    use crate::to_bits;
+    use crate::{to_bits, to_chars};
     use crate::other::create_correct_vec;
 
     #[test]
@@ -28,5 +64,14 @@ mod test {
         let correct = create_correct_vec();
         let image = format!("{:#?}", to_bits("./cuddlyferris.png").expect("could not process image"));
         assert_eq!(correct, image);
+    }
+
+    #[test]
+    fn to_chars_test() {
+        let bits = to_bits("./cuddlyferris.png").expect("could not process image");
+
+        let correct: Vec<char> = Vec::new();
+        let result = to_chars(bits);
+        assert_eq!(correct, result);
     }
 }
